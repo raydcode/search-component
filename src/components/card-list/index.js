faf0b49af1639e23e1271eba8dddabe7354a6354
kathir2602 <kathir@crayond.co>
2022-02-14 19:40:00 +0530

    init

--- src/components/card-list/index.js
+++ src/components/card-list/index.js
@@ -7,9 +7,18 @@ import './card-list.css';
 const CardList = ({ users }) => {
   return (
     <div className="card-list">
-      {users.map(user => (
-        <Card key={user.id} id={user.id} name={user.name} email={user.email} />
-      ))}
+      {users.length > 0 ? (
+        users.map((user) => (
+          <Card
+            key={user.id}
+            id={user.id}
+            name={user.name}
+            email={user.email}
+          />
+        ))
+      ) : (
+        <h1 style={{ textAlign: 'center' }}>No Users Found !!!</h1>
+      )}
     </div>
   );
 };
